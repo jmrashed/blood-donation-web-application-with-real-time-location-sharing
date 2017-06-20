1851a5c84b55129f342efa52cda98499a55638b8
Rasheduzzaman <Rasheduzzaman>
2017-06-21 00:57:34 +0600

    all

---
+++ storage/framework/views/28d79b92daecb8e934e7a77ec8b061e27d4a02c3.php
@@ -0,0 +1,57 @@
+<?php $__env->startSection('content'); ?>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Main content -->
+    <section class="content">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="box">
+
+                    <div class="box-header with-border">
+                        <h3 class="box-title"> </h3>
+
+                        <div class="box-tools pull-right">
+
+                            <a class="btn btn-success pull-right" href="<?php echo e(url('blog/createCategory')); ?>"><i class="fa fa-plus"></i> Create Category</a>
+
+                        </div>
+                        <h3 class="box-title">Admin List</h3>
+
+                    </div>
+
+                    <!-- /.box-header -->
+                    <div class="box-body">
+
+                        <table id="example1" class="table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Category ID</th>
+                                    <th>Category Name</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php $__currentLoopData = $data; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $row): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                <tr>
+                                    <td><?php echo e($row->id); ?></td>
+                                    <td><?php echo e($row->category_name); ?></td>
+                                </tr>
+
+                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                            </tbody>
+
+                        </table>
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
+
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+<?php $__env->stopSection(); ?> 
+<?php echo $__env->make('layouts.myapp', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
