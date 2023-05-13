37d7006dad17001f235766968e43ea6d22e63a23
Foysal Ahammad <foysal.int@gmail.com>
2023-05-13 10:02:58 +0600

    Update conposer.json

--- config/app.php
+++ config/app.php
@@ -162,7 +162,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Zizaco\Entrust\EntrustServiceProvider::class,
+        // Zizaco\Entrust\EntrustServiceProvider::class,
         /*
          * Package Service Providers...
          */
